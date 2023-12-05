75ccd316980d717d00b063602817a05a225427e2
Tim Philips <timp@unfuddle.nz>
2023-12-06 10:49:06 +1300

    Remove debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,9 @@
-#!/bin/sh -x
+#!/bin/sh -e
 
 GITHUB_REPOSITORY=$(echo "$GITHUB_REPOSITORY" | cut -d"/" -f2)
 PULL_REQUEST_ID=$(echo "$GITHUB_REF_NAME" | cut -d"/" -f1)
 
-set
-
 /usr/local/bin/cdk-notifier \
-  --ci github \
   --delete "$1" \
   --log-file "$2" \
   --owner "$GITHUB_REPOSITORY_OWNER" \
